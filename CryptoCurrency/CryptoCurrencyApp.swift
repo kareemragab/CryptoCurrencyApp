d26602bd6ed2d335b34945b2e52c62db5f9ec4d4
kareem Hassan <kragab@nu.edu.eg>
2023-12-22 19:05:55 +0200

    Initial Commit

---
+++ CryptoCurrency/CryptoCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyApp.swift
+//  CryptoCurrency
+//
+//  Created by Kareem Ragab Hassan on 22/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
